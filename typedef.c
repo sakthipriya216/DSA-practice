17a40f41c685d6918a6f7fe945085e8e376a2a13
sakthipriya216 <119375007+sakthipriya216@users.noreply.github.com>
2023-02-09 13:23:18 +0530

    Create typedef.c

---
+++ typedef.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct Node
+{
+    int data;
+    struct Node* next;
+} node;
+
+// using type def we can shorten the keywords. It is used to provide new names for exisiting datatypes
+node* head=NULL;
+node* createNode()
+{
+    node* temp= (node*) malloc(sizeof(node*));
+    printf("Enter the data: ");
+    // populating the data inside the structure variable
+    scanf("%d",&temp->data);
+    temp->next=NULL;
+    return temp;
+}
+
+int main()
+{
+    node* th=createNode();
+    printf("%d %p",th->data,th->next);
+    return 0;
+}
